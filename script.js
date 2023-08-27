8c0bd491b44e9ecd1ffa0f314580f84b998fdfd6
Alyssonbfr <alyssonvictor2002@gmail.com>
2023-08-27 05:26:21 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,126 @@
+
+
+const estadoSelect = document.getElementById('estadoSelect')
+const cidadeSelect = document.getElementById('cidadeSelect')
+
+// Função para preencher as opções de estados
+// Função para preencher as opções de estados em ordem alfabética
+async function carregarEstados() {
+    const response = await fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados');
+    const estados = await response.json()
+
+    estados.sort((a, b) => a.nome.localeCompare(b.nome)) // Ordena estados alfabeticamente
+
+    estados.forEach(estado => {
+        const optionElement = document.createElement('option')
+        optionElement.value = estado.sigla
+        optionElement.textContent = estado.nome
+        estadoSelect.appendChild(optionElement)
+    });
+}
+
+// Função para preencher as opções de cidades com base no estado selecionado
+async function carregarCidadesPorEstado(uf) {
+    const response = await fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${uf}/municipios`);
+    const cidades = await response.json();
+
+    cidadeSelect.innerHTML = '<option value="" class="option">Selecione uma cidade</option>';
+    cidades.forEach(cidade => {
+        const optionElement = document.createElement('option')
+        optionElement.value = cidade.nome
+        optionElement.textContent = cidade.nome
+        cidadeSelect.appendChild(optionElement)
+    });
+}
+
+// Carrega os estados e adiciona um ouvinte de evento para mudanças no estado selecionado
+carregarEstados();
+estadoSelect.addEventListener('change', function () {
+    const ufSelecionado = estadoSelect.value;
+    if (ufSelecionado) {
+        carregarCidadesPorEstado(ufSelecionado)
+    } else {
+        cidadeSelect.innerHTML = '<option value="" class="option">Selecione um estado primeiro</option>';
+    }
+
+    function highlight_map_states(){
+        if($(".states_section").length>0){
+           $(".states_section .list_states .item .link").hover(function(){
+             var a="#state_"+$(this).text().toLowerCase();
+             $(a).attr("class","state hover")
+           },function(){
+             var a="#state_"+$(this).text().toLowerCase();
+             $(a).attr("class","state")
+           })
+        }
+      };
+});
+
+function mostrarDados(dados){
+    console.log(dados)
+    document.querySelector(".city").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".temp").innerHTML =  Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".humidity").innerHTML = "Umidade: " + dados.main.humidity + "%"
+    document.querySelector(".mensage").innerHTML = dados.weather[0].description
+    document.querySelector(".sunrise").innerHTML = "Nascer do Sol: " + converterTimestamp(dados.sys.sunrise)
+    document.querySelector(".sunset").innerHTML = "Por do Sol: " + converterTimestamp(dados.sys.sunset)
+    
+    
+    var smallBoxElement = document.querySelector(".smallbox") // Seleciona o elemento com a classe "smallbox"
+    
+    // Verifica se já existe uma imagem dentro do elemento
+    var existingImage = smallBoxElement.querySelector("img")
+        
+    if (existingImage) {
+        smallBoxElement.removeChild(existingImage) // Remove a imagem existente, se houver
+    }
+
+    var newImage = document.createElement("img");
+    newImage.src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"; // Use a URL da imagem dinâmica
+    newImage.alt = "Condições climáticas"
+
+    // Insere a imagem antes do primeiro elemento filho (o parágrafo com a classe "message")
+    smallBoxElement.insertBefore(newImage, smallBoxElement.firstChild)
+}
+
+function converterTimestamp(timestamp){
+    const data = new Date(timestamp*1000)
+    const horas = data.getHours()
+    const minutos = data.getMinutes()
+    const segundos = data.getSeconds()
+
+    const horasFormatadas = horas < 10 ? `0${horas}` : horas
+    const minutosFormatados = minutos < 10 ? `0${minutos}` : minutos
+    const segundosFormatados = segundos < 10 ? `0${segundos}` : segundos
+
+    const horario = horasFormatadas+":"+minutosFormatados+":"+segundosFormatados
+    return horario
+}
+
+const key = "e406f26cf5a7cfcb04b34d595476dff6" 
+
+async function buscarId(cidade) {
+    let dados = await fetch("http://apiadvisor.climatempo.com.br/api/v1/locale/city?name=" + cidade +"&state=RJ&token=a9255f48ab3ba44a489b9418b0ee24d3").then(resposta => resposta.json())
+
+    console.log(dados[0].id)
+}
+
+async function buscar(cidade) {
+    let dados = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + 
+    cidade + 
+    "&appid=" + 
+    key + 
+    "&lang=pt_br" +
+    "&units=metric"
+    )
+    .then(resposta => resposta.json())
+    
+    mostrarDados(dados)
+}
+
+function cliqueNoBotao(){
+    const cidade = document.getElementById("cidadeSelect").value
+    console.log(cidade)
+    buscar(cidade)
+
+}
